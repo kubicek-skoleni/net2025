728001dcb9eb7df49d15f7e263d898d7f6a2c0d3
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-24 14:24:19 +0200

    LINQ cars

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -1,43 +1,10 @@
-﻿
-var people = new[]
+﻿var cars = new[]
 {
-     new { Name = "Jan", City = "Praha", Age = 25 },
-     new { Name = "Marie", City = "Brno", Age = 30 },
-     new { Name = "Petr", City = "Praha", Age = 35 },
-     new { Name = "Eva", City = "Brno", Age = 28 },
-     new { Name = "Tomáš", City = "Ostrava", Age = 32 },
-     new { Name = "Lucie", City = "Praha", Age = 27 }
+    new { Brand = "Škoda", Model = "Octavia", Year = 2020, PricePerDay = 800, Available = true },
+    new { Brand = "Škoda", Model = "Fabia", Year = 2021, PricePerDay = 600, Available = true },
+    new { Brand = "VW", Model = "Golf", Year = 2019, PricePerDay = 900, Available = false },
+    new { Brand = "VW", Model = "Passat", Year = 2020, PricePerDay = 1200, Available = true },
+    new { Brand = "BMW", Model = "3 Series", Year = 2021, PricePerDay = 1500, Available = true },
+    new { Brand = "BMW", Model = "X3", Year = 2020, PricePerDay = 2000, Available = false }
 };
 
-//var result = people.GroupBy(x => x.City);
-
-//foreach (var city_people in result)
-//{
-//    Console.WriteLine($"Key: {city_people.Key}");
-
-//    foreach(var person in city_people)
-//    {
-//        Console.WriteLine(person.Name);
-//    }
-
-//    Console.WriteLine();
-//}
-
-// Seskup podle věkové kategorie (pod 30 a 30+)
-// kolik v každé skupině
-// vypiš lidi dle skupin
-
-var byAgeCategory = people.GroupBy(x => x.Age < 30 ? "Pod 30" : "30+");
-
-foreach(var ageGroup in  byAgeCategory)
-{
-    var group_name = ageGroup.Key;
-    var group_count = ageGroup.Count();
-
-    Console.WriteLine($"skupina {group_name}, počet lidí: {group_count}");
-
-    foreach(var person in ageGroup)
-    {
-        Console.WriteLine($"{person.Name} - {person.Age}");
-    }
-}
