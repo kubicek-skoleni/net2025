e27bc909c5516d2ce843ab3b526ff1cba63ab195
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-24 13:17:16 +0200

    prnich 6 ukolu

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -2,3 +2,10 @@
 
 int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+// 1. Najdi všechna čísla menší než 5
+// 2. Najdi všechna sudá čísla
+// 3. Seřaď čísla vzestupně
+// 4. Najdi slova kratší než 4 znaky
+// 5. Vezmi prvních 5 čísel
+// 6. Přeskoč první 3 čísla a vezmi zbytek
