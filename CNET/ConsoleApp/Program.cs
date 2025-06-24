88d8e889d341d1111cc91f0ba9dab6d77c7a592e
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-24 14:22:43 +0200

    group by cviceni vysledek

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -9,20 +9,35 @@ var people = new[]
      new { Name = "Lucie", City = "Praha", Age = 27 }
 };
 
-var result = people.GroupBy(x => x.City);
+//var result = people.GroupBy(x => x.City);
 
-foreach (var city_people in result)
-{
-    Console.WriteLine($"Key: {city_people.Key}");
+//foreach (var city_people in result)
+//{
+//    Console.WriteLine($"Key: {city_people.Key}");
 
-    foreach(var person in city_people)
-    {
-        Console.WriteLine(person.Name);
-    }
+//    foreach(var person in city_people)
+//    {
+//        Console.WriteLine(person.Name);
+//    }
 
-    Console.WriteLine();
-}
+//    Console.WriteLine();
+//}
 
 // Seskup podle věkové kategorie (pod 30 a 30+)
 // kolik v každé skupině
 // vypiš lidi dle skupin
+
+var byAgeCategory = people.GroupBy(x => x.Age < 30 ? "Pod 30" : "30+");
+
+foreach(var ageGroup in  byAgeCategory)
+{
+    var group_name = ageGroup.Key;
+    var group_count = ageGroup.Count();
+
+    Console.WriteLine($"skupina {group_name}, počet lidí: {group_count}");
+
+    foreach(var person in ageGroup)
+    {
+        Console.WriteLine($"{person.Name} - {person.Age}");
+    }
+}
