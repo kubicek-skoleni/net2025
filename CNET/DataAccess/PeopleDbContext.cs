dce9a6e6f61d06dde0a81ab51a567d899cb0d19a
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-25 10:25:39 +0200

    EF core + init

---
+++ CNET/DataAccess/PeopleDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace DataAccess
+{
+    public class PeopleDbConxtext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<Contract> Contracts { get; set; }
+
+        public string DbPath { get; }
+
+        public PeopleDbConxtext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "people.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+}
