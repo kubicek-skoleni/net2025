e8445aee8353c2cfa2b405e2295e4806cac7c74a
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-25 13:47:29 +0200

    basick http call from console to api

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -1,1 +1,9 @@
 ﻿
+Console.WriteLine("Starting..");
+
+HttpClient client = new();
+
+
+var homeText = await client.GetStringAsync("https://localhost:7194");
+
+Console.WriteLine($"result: {homeText}");
