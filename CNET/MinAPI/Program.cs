4d5cb78d057ad98a10b56e04e17d0abac0ee8290
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-25 12:57:49 +0200

    Min API

---
+++ CNET/MinAPI/Program.cs
@@ -0,0 +1,28 @@
+using DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<PeopleDbConxtext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello from API");
+
+app.MapGet("/person/count",
+            (PeopleDbConxtext db) => db.Persons.Count());
+
+app.Run();
+
+
