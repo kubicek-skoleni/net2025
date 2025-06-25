1634af5634e74258990bc8430e122262f9fc0623
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-25 14:22:12 +0200

    console while

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -3,29 +3,30 @@ using Model;
 
 Console.WriteLine("Starting..");
 
-Console.WriteLine("Zadej ID osoby:");
-string line = Console.ReadLine();
-int id = 0;
-
-try
-{
-    id = int.Parse(line);
-}
-catch(Exception ex)
-{
-    Console.WriteLine("Nepodařilo se mi převést ID na číslo");
-    return;
-}
-
-
 HttpClient client = new();
 client.BaseAddress = new Uri("https://localhost:7194");
-Person person = await client.GetFromJsonAsync<Person>($"/person/{id}");
 
+int id = 0;
 
-Console.WriteLine($"person id {id}: {person.FirstName} {person.LastName}");
+while (true)
+{
+    Console.Write("Input Person ID: ");
+    var consoleInput = Console.ReadLine(); // get input from user
 
-//1. upravte aplikaci, aby se ptala opakovaně v cyklu na id
-// zobrazí informaci o osobě
-// nebo informaci že se nepodařilo převést ID
-// končí když uživatel napíše "Q"
+    if (consoleInput == "Q")
+        break;
+
+    try
+    {
+        id = int.Parse(consoleInput); // parse user input (string) to integer
+    }
+    catch (Exception)
+    {
+
+        Console.WriteLine("Invalid input!");
+        continue;
+    }
+
+    var person = await client.GetFromJsonAsync<Person>($"/person/{id}");
+    Console.WriteLine($"Person with ID {id}: {person.FirstName} {person.LastName}");
+}
