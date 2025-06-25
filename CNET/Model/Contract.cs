220a6642b82b127becaedf366cae637e6458a009
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-25 09:22:43 +0200

    model - person / json dataset

---
+++ CNET/Model/Contract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Contract
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PlateNumber { get; set; }
+        public DateTime Signed { get; set; }
+        public int CarBrand { get; set; }
+        public string HexColor { get; set; }
+    }
+}
---
+++ CNET/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Model;
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public Address Address { get; set; }
+    public ICollection<Contract> Contracts { get; set; }
+}
+
