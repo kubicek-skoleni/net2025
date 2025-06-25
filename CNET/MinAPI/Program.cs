8cf48cff4c1bea1d2d51f5f9a5a373f39cbd4908
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-25 13:31:10 +0200

    api /person/email hledani

--- CNET/MinAPI/Program.cs
+++ CNET/MinAPI/Program.cs
@@ -1,4 +1,4 @@
-using DataAccess;
+﻿using DataAccess;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,6 +30,28 @@ app.MapGet("/person/{id}", (int id, PeopleDbConxtext db)
     .Include(x => x.Contracts)
     .Where(person => person.Id == id).First()
 );
+
+app.Map("/contract/count", (PeopleDbConxtext db)
+    => db.Contracts.Count());
+
+app.MapGet("/city/top20", (PeopleDbConxtext db) =>
+        db.Persons
+        .Include(x => x.Address)
+        .Where(x => x.Address != null)
+        .GroupBy(x => x.Address.City)
+        .ToList()
+        .OrderByDescending(g => g.Count())
+        .Take(20)
+        .Select(x => new { City = x.Key, Count = x.Count() })
+        );
+
+// hledání osob podle emailu (část emailu, - obsahuje)
+
+app.MapGet("/person/email/{email}", (string email, PeopleDbConxtext db)
+    => db.Persons
+    .Where(x => x.Email.ToLower().Contains(email.ToLower()))
+    .Take(100)
+    );
 
 app.Run();
 
