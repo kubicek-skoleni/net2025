3207ee86b0a9b69fb49bc99bcab8d123a178768c
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-25 13:57:40 +0200

    cosole - ready person by id

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -1,9 +1,27 @@
 ﻿
+using System.Net.Http.Json;
+using Model;
+
 Console.WriteLine("Starting..");
 
+Console.WriteLine("Zadej ID osoby:");
+string line = Console.ReadLine();
+int id = 0;
+
+try
+{
+    id = int.Parse(line);
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Nepodařilo se mi převést ID na číslo");
+    return;
+}
+
+
 HttpClient client = new();
+client.BaseAddress = new Uri("https://localhost:7194");
+Person person = await client.GetFromJsonAsync<Person>($"/person/{id}");
 
 
-var homeText = await client.GetStringAsync("https://localhost:7194");
-
-Console.WriteLine($"result: {homeText}");
+Console.WriteLine($"person id {id}: {person.FirstName} {person.LastName}");
