e166d665d842e73f6411086f3b1ac07ad350f729
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-25 11:44:01 +0200

    LINQ nad databazi

--- CNET/ConsoleApp/Program.cs
+++ CNET/ConsoleApp/Program.cs
@@ -3,23 +3,33 @@ using Microsoft.EntityFrameworkCore;
 
 // připojte se k db
 
-// 1. města a kolik v každěm městě osob a vypište do konzole
-
 var db = new PeopleDbConxtext();
 
-
+// 1. města a kolik v každěm městě osob a vypište do konzole
 // řešení bez vazby
 //var cities = db.Addresses.GroupBy(x => x.City);
 
 // řešení s vazbou
+//var cities = db.Persons
+//    .Include(x => x.Address)
+//    .Where(x => x.Address != null)
+//    .GroupBy(x => x.Address.City);
+
+// 2. upravte na pouze 20 nejčastěších měst
 var cities = db.Persons
     .Include(x => x.Address)
     .Where(x => x.Address != null)
-    .GroupBy(x => x.Address.City);
+    .GroupBy(x => x.Address.City)
+    .ToList()
+    .OrderByDescending(g => g.Count())
+    .Take(20)
+    ;
 
 foreach (var city in cities)
 {
     Console.WriteLine($"město: {city.Key}, počet lidí: {city.Count()}");
 }
+
+//3. kolik je v systému smluv (Contracts)
 
 
