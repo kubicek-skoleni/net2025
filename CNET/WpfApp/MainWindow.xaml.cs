a1f3c836dbbe07c86c928481bb176bd8d4dd79e7
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-26 12:56:30 +0200

    global stats

--- CNET/WpfApp/MainWindow.xaml.cs
+++ CNET/WpfApp/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace WpfApp
     {
         private string baseUrl = "https://localhost:7194";
         private HttpClient client = new();
-        
+
 
         public MainWindow()
         {
@@ -39,14 +39,24 @@ namespace WpfApp
             client.BaseAddress = new Uri(baseUrl);
         }
 
+        private List<string> LoadFiles()
+        {
+            string dir = @"C:\PROJECTS\skoleni2025\github\net2025\bigfiles";
+
+            List<string> files = Directory.EnumerateFiles(dir, "*.txt")
+                                         .ToList();
+
+            return files;
+        }
+
         private async void btnTest_Click(object sender, RoutedEventArgs e)
         {
             var top20 = await client.GetFromJsonAsync<List<CityInfo>>("/city/top20");
 
             txbInfo.Text = "";
-            
 
-            foreach (var cityInfo in top20) 
+
+            foreach (var cityInfo in top20)
             {
                 txbInfo.Text += $"{cityInfo.City}: {cityInfo.Count} {Environment.NewLine}";
             }
@@ -90,11 +100,62 @@ namespace WpfApp
                 var lines = File.ReadLines(file).ToArray();
                 var count = lines.Count();
 
-                txbInfo.Text += $"{file} : {count} {Environment.NewLine} ";    
+                txbInfo.Text += $"{file} : {count} {Environment.NewLine} ";
             }
 
             stopwatch.Stop();
             txbInfo.Text += $"elapsed ms: {stopwatch.ElapsedMilliseconds}";
         }
+
+        private void btnProcessFilesAll_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+               10 nejcastejsich slov celkem ve všech souborech - globálně
+            */
+
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            txbInfo.Text = "";
+
+            Dictionary<string, int> stats = new();
+
+            var files = LoadFiles();
+
+            foreach (var file in files)
+            {
+                var words = File.ReadLines(file);
+
+                foreach (var word in words)
+                {
+                    if (stats.ContainsKey(word))
+                        stats[word]++;
+                    else
+                        stats.Add(word, 1);
+                }
+            }
+
+            var top10 = stats.OrderByDescending(x => x.Value).Take(10);
+
+            foreach (var word_kv in top10)
+            {
+                txbInfo.Text += $"{word_kv.Key}: {word_kv.Value} {Environment.NewLine}";
+            }
+
+            stopwatch.Stop();
+            txbInfo.Text += $"elapsed ms: {stopwatch.ElapsedMilliseconds}";
+
+            Mouse.OverrideCursor = null;
+        }
+
+
+        //var stopwatch = new Stopwatch();
+        //stopwatch.Start();
+        //txbInfo.Text = "";
+
+        //stopwatch.Stop();
+        //txbInfo.Text += $"elapsed ms: {stopwatch.ElapsedMilliseconds}";
+
     }
 }
