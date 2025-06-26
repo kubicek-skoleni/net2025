1f534d12c0bccc2e6e65b903d03daac3335b2081
kubicek-skoleni <kubicek-brno@gmail.com>
2025-06-26 10:43:35 +0200

    WPF - person by id call

--- CNET/WpfApp/MainWindow.xaml.cs
+++ CNET/WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Buffers.Text;
+using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text;
 using System.Windows;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Model;
 
 namespace WpfApp
 {
@@ -24,17 +26,19 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string baseUrl = "https://localhost:7194";
+        private HttpClient client = new();
+        
+
         public MainWindow()
         {
             InitializeComponent();
+
+            client.BaseAddress = new Uri(baseUrl);
         }
 
         private async void btnTest_Click(object sender, RoutedEventArgs e)
         {
-            var baseUrl = "https://localhost:7194";
-            HttpClient client = new();
-            client.BaseAddress = new Uri(baseUrl);
-
             var top20 = await client.GetFromJsonAsync<List<CityInfo>>("/city/top20");
 
             txbInfo.Text = "";
@@ -43,6 +47,26 @@ namespace WpfApp
             {
                 txbInfo.Text += $"{cityInfo.City}: {cityInfo.Count} {Environment.NewLine}";
             }
+        }
+
+        private async void btnPersonDetail_Click(object sender, RoutedEventArgs e)
+        {
+            int id = 0;
+            bool result = int.TryParse(txtPersonID.Text, out id);
+
+            if (!result)
+            {
+                txbInfo.Text = "Nepodařilo se převést ID na int";
+                return;
+            }
+
+            var person =
+                await client.GetFromJsonAsync<Person>($"/person/{id}");
+
+            txbInfo.Text = "";
+
+            txbInfo.Text =
+                $"{person.FirstName} {person.LastName} {person.Email}";
         }
     }
 }
